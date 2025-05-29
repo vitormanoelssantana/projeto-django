152e15c60ded1fe2bdd7be7ce54e79b84b10323e
vitormanoelssantana <vitormanoels.santana@gmail.com>
2025-05-29 11:10:10 -0300

    pessoa adicionada

--- alunos/admin.py
+++ alunos/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Estado 
+from .models import Estado, Cidade, Pessoa
 
 admin.site.register(Estado)
+admin.site.register(Cidade)
+admin.site.register(Pessoa)
--- alunos/models.py
+++ alunos/models.py
@@ -5,4 +5,21 @@ class Estado(models.Model):
     sigla = models.CharField(max_length=2)
 
     def __str__(self):
-        return self.nome 
+        return f'{self.nome} ({self.sigla})'
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.nome} ({self.estado.sigla})'
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50)
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+    dataDeNascimento = models.DateField(verbose_name="Data de nascimento")
+    email = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.nome}'
